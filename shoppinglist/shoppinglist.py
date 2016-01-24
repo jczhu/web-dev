e9af244b38ae87735b495cd9752fbc9a9504b57e
Julie Zhu <juliez@princeton.edu>
2016-01-23 23:36:17 -0500

    Add template, problem with fizzbuzz rendering (html ok)

--- shoppinglist/shoppinglist.py
+++ shoppinglist/shoppinglist.py
@@ -1,15 +1,11 @@
 import os
 
+import jinja2
 import webapp2
 
-form_html = """
-<form>
-<h2>Add Food</h2>
-<input type="text" name="food">
-%s
-<button>Add</button>
-</form>
-"""
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 hidden_html = """
 <input type="hidden" name="food" value="%s">
@@ -29,24 +25,39 @@ class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.write(*a, **kw)
 
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
 class MainPage(Handler):
 	def get(self):
-		output = form_html
-		output_hidden = ""
+		self.render("shopping_list.html")
 
-		items = self.request.get_all("food")
-		if items:
-			output_items = ""
-			for item in items:
-				output_hidden += hidden_html % item
-				output_items += item_html % item
+		# output = form_html
+		# output_hidden = ""
 
-			output_shopping = shopping_list_html % output_items
-			output += output_shopping
+		# items = self.request.get_all("food")
+		# if items:
+		# 	output_items = ""
+		# 	for item in items:
+		# 		output_hidden += hidden_html % item
+		# 		output_items += item_html % item
 
-		output = output % output_hidden
-		self.write(output)
+		# 	output_shopping = shopping_list_html % output_items
+		# 	output += output_shopping
+
+		# output = output % output_hidden
+		# self.write(output)
+
+# class FizzBuzzHandler(Handler):
+# 	def get(self):
+# 		n = self.request.get('n', 0)
+# 		n = n and int(n)
+# 		self.render('fizzbuzz.html', n = n)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage), ('/fizzbuzz', FizzBuzzHandler)
 ], debug=True)
