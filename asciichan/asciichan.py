b67fdb86fba73326f27f4aaedae21403cdba68ec
Julie Zhu <juliez@princeton.edu>
2016-01-24 22:52:43 -0500

    Output art from database and add styles

--- asciichan/asciichan.py
+++ asciichan/asciichan.py
@@ -30,7 +30,10 @@ class Art(db.Model):
 
 class MainPage(Handler):
 	def render_front(self, title="", art="", error=""):
-		self.render("front.html", title=title, art=art, error=error)
+		arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+
+
+		self.render("front.html", title=title, art=art, error=error, arts = arts)
 
 	def get(self):
 		self.render_front()
@@ -40,7 +43,10 @@ class MainPage(Handler):
 		art = self.request.get("art")
 
 		if title and art:
-			self.write("thanks!")
+			a = Art(title = title, art = art)
+			a.put()
+
+			self.redirect("/")
 		else:
 			error = "we need both a title and some artwork"
 			self.render_front(title, art, error)
